dd7a5ca8b76dc2242123d64a49f1221a6622020b
Tobias Kapp <tobias.kapp@diconium.com>
2022-03-18 12:20:48 +0100

    default Profile angelegt

--- src/main/java/tobi/inc/sfgdi/Services/I18nSpanishGreetingService.java
+++ src/main/java/tobi/inc/sfgdi/Services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package tobi.inc.sfgdi.Services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService{
     @Override
